837b883cea80db253459968cb5279a9e42c688e1
Sai Prashant Saxena <saiprashant.saxena@gmail.com>
2023-03-08 22:50:56 +0530

    tailwind setup bugfix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 function App() {
   return (
-    <div className="">Hi</div>
+    <div className="text-center">Hi</div>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
     colors: {
       "green-primary": "#4CAF50",
