e8b434d9d64df56bd43af9069f101d3608580e90
Sai Prashant Saxena <saiprashant.saxena@gmail.com>
2023-03-08 22:44:40 +0530

    tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    colors: {
+      "green-primary": "#4CAF50",
+      "green-light": "#00A1072B",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
